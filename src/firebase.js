fcfd3a8b6e81222329d3b657fb98383f33fd6697
Pushpinder Singh <pushpinder.singh@duneshift.com>
2022-01-15 00:05:55 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWipoEEi32UkrKepT1z449qmsE8SXCssg",
+    authDomain: "netflix-clone-ee29b.firebaseapp.com",
+    projectId: "netflix-clone-ee29b",
+    storageBucket: "netflix-clone-ee29b.appspot.com",
+    messagingSenderId: "436235005962",
+    appId: "1:436235005962:web:54708d784ce0aacd662ff3"
+  };
