46c9bab9bff8f1d338e04af9bda1f23016fea418
Pushpinder Singh <pushpinder.singh@duneshift.com>
2022-01-22 00:10:51 +0530

    feat: Authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
 
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBWipoEEi32UkrKepT1z449qmsE8SXCssg",
-    authDomain: "netflix-clone-ee29b.firebaseapp.com",
-    projectId: "netflix-clone-ee29b",
-    storageBucket: "netflix-clone-ee29b.appspot.com",
-    messagingSenderId: "436235005962",
-    appId: "1:436235005962:web:54708d784ce0aacd662ff3"
-  };
+  apiKey: "AIzaSyBWipoEEi32UkrKepT1z449qmsE8SXCssg",
+  authDomain: "netflix-clone-ee29b.firebaseapp.com",
+  projectId: "netflix-clone-ee29b",
+  storageBucket: "netflix-clone-ee29b.appspot.com",
+  messagingSenderId: "436235005962",
+  appId: "1:436235005962:web:54708d784ce0aacd662ff3"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
